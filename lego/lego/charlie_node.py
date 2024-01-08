18a6707b4d00c4c6622c5c195a09353bcf818be7
Julian Francis <ubuntu@ubuntu.cust.communityfibre.co.uk>
2024-01-08 15:52:52 +0000

    Charlie broadcasts joint_state messages

--- lego/lego/charlie_node.py
+++ lego/lego/charlie_node.py
@@ -1,10 +1,10 @@
 """CharlieNode listens to Twist messages and drives Charlie the robot"""
-import time
 import math
 import brickpi3
 import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Twist
+from sensor_msgs.msg import JointState
 
 
 class CharlieNode(Node):
@@ -12,21 +12,27 @@ class CharlieNode(Node):
     def __init__(self):
         super().__init__('charlie_the_robot')
         self.bp = brickpi3.BrickPi3()
+        self.off()
         self.subscription = self.create_subscription(
             Twist,
             'cmd_vel',
             self.listener_callback, 0)
-        self.off()
+        self.publisher = self.create_publisher(JointState, "/joint_states", 10)
         self.wheel_radius = 0.02
         self.turning_circle_radius = 0.132/2.0
-        timer_period = 0.2
-        self.timer = self.create_timer(timer_period, self.timer_callback)
-        self.last_command_time = time.time()
+        self.declare_parameter('joint_states_publish_frequency', 5.0)
+        joint_states_timer_period = \
+            1.0/self.get_parameter('joint_states_publish_frequency'). \
+            get_parameter_value().double_value
+        self.joint_states_times = \
+            self.create_timer(joint_states_timer_period, self.joint_states_callback)
+        self.declare_parameter('timeout', 1.0)
+        timeout = self.get_parameter('timeout').get_parameter_value().double_value
+        self.timeout_timer = self.create_timer(timeout, self.timeout_callback)
         self.get_logger().info('Robot ready.')
 
     def listener_callback(self, msg):
         """sends lego drive commands to robot in response to Twist messages"""
-        self.last_command_time = time.time()
         translation_speed_dps = (360.0/(2*math.pi))*msg.linear.x/self.wheel_radius
         rotation_speed_dps = (360.0/(2*math.pi))* \
             msg.angular.z*self.turning_circle_radius/self.wheel_radius
@@ -34,12 +40,22 @@ class CharlieNode(Node):
         motord_dps = translation_speed_dps + rotation_speed_dps
         self.bp.set_motor_dps(self.bp.PORT_A, motora_dps)
         self.bp.set_motor_dps(self.bp.PORT_D, motord_dps)
+        self.timeout_timer.reset()
 
-    def timer_callback(self):
+    def joint_states_callback(self):
+        """publish the positions of the wheels"""
+        msg = JointState()
+        msg.header.stamp = self.get_clock().now().to_msg()
+        msg.header.frame_id = "charlie"
+        msg.name = ["left_wheel", "right_wheel"]
+        left_wheel_pos = float(self.bp.get_motor_encoder(self.bp.PORT_A))
+        right_wheel_pos = float(self.bp.get_motor_encoder(self.bp.PORT_D))
+        msg.position = [left_wheel_pos, right_wheel_pos]
+        self.publisher.publish(msg)
+
+    def timeout_callback(self):
         """stop the robot if no Twist message received within timeout"""
-        timeout = .5
-        if (time.time() - self.last_command_time) > timeout:
-            self.off()
+        self.off()
 
     def off(self):
         """stop the robot"""
