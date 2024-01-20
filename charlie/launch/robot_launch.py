4b13504f0e9949a5ce23a037b8ad68539b0d72c9
Julian Francis <ubuntu@ubuntu.cust.communityfibre.co.uk>
2024-01-20 08:46:00 +0000

    The brickpi3 packages are now in brickpi3_sensors and brickpi3_motors

--- charlie/launch/robot_launch.py
+++ charlie/launch/robot_launch.py
@@ -1,18 +1,28 @@
 from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import PathJoinSubstitution
 from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
 
 def generate_launch_description():
+    launch_brickpi3_charlie = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([
+            PathJoinSubstitution([
+                FindPackageShare('brickpi3_charlie'),
+                'launch',
+                'brickpi3_motors_launch.py'
+            ])
+        ]))
     return LaunchDescription([
+        launch_brickpi3_charlie,
         Node(
-            package='ev3_pi',
-            executable='differential_drive_node',
-        ),
-        Node(
-            package='ev3_pi',
+            package='brickpi3_sensors',
             executable='compass_node',
         ),
         Node(
-            package='ev3_pi',
+            package='brickpi3_sensors',
             executable='ultrasonic_distance_node',
             parameters=[{"lego_port": "PORT_2"}]
         ),
