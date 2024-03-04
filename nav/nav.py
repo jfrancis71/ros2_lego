1235a4ede5d175b9d599314612ec1db48c5396f1
Julian Francis <julian.w.francis@gmail.com>
2024-03-04 14:37:58 +0000

    inertial guidance seems to be working quite well (doesn't work well on carpet!!!)

--- nav/nav.py
+++ nav/nav.py
@@ -1,5 +1,7 @@
 import collections
 import math
+import numpy as np
+import scipy
 
 from vision_msgs.msg import Detection2DArray
 from vision_msgs.msg import BoundingBox2D
@@ -9,6 +11,7 @@ import rclpy
 from rclpy.node import Node
 import torch
 from scipy.spatial.transform import Rotation as R
+from tf_transformations import euler_from_quaternion
 
 
 
@@ -70,8 +73,15 @@ class Nav(Node):
         area_ratio = torch.nan_to_num(area_ratio, nan=0.0)
         self.proba = 0.05 * (1-area_ratio) + 0.95 * area_ratio
         self.current_probability_map = \
-            torch.zeros([self.num_grid_cells, self.num_grid_cells, self.num_orientation_cells]) + \
-            (1.0/(self.num_grid_cells*self.num_grid_cells*self.num_orientation_cells))
+            torch.zeros([self.num_grid_cells, self.num_grid_cells, self.num_orientation_cells])
+#             + \
+#            (1.0/(self.num_grid_cells*self.num_grid_cells*self.num_orientation_cells)))
+        self.current_probability_map[int(self.num_grid_cells/2), int(self.num_grid_cells/2), 0] = 1.0
+        self.last_inertial_position = None
+        self.position_kernel = np.zeros([11,11])
+        self.orientation_kernel = np.zeros([self.num_orientation_cells])
+        self.position_kernel[5,5] = 1.0
+        self.orientation_kernel[0] = 1.0
 
     def world_to_camera(self, world_point):
         world_translate_x = world_point.x - self.world_x
@@ -167,13 +177,51 @@ class Nav(Node):
         self.pose_publisher.publish(pose_stamped)
 
     def detections_callback(self, msg):
-        self.pose_from_detections_probability_map = self.probmessage(msg.detections)
-        self.publish_occupancy_grid_msg(self.pose_from_detections_probability_map, msg.header)
-        self.publish_pose_msg(self.pose_from_detections_probability_map, msg.header)
-        print("loc")
+#        self.pose_from_detections_probability_map = self.probmessage(msg.detections)
+#        self.publish_occupancy_grid_msg(self.pose_from_detections_probability_map, msg.header)
+#        self.publish_pose_msg(self.pose_from_detections_probability_map, msg.header)
+#        print("loc")
+        pass
 
     def pose_callback(self, msg):
-        print("POSE")
+        print("POSE", msg.pose.pose.position)
+        current_inertial_position = torch.tensor([msg.pose.pose.position.x, msg.pose.pose.position.y])
+        q = msg.pose.pose.orientation
+
+        current_inertial_orientation = -euler_from_quaternion((q.x, q.y, q.z, q.w))[2]
+        print("o=", current_inertial_orientation)
+        if self.last_inertial_position is None:
+            self.last_inertial_position = current_inertial_position
+            self.last_inertial_orientation = current_inertial_orientation
+            return
+        inertial_position_difference = self.last_inertial_position - current_inertial_position
+        inertial_orientation_difference = self.last_inertial_orientation - current_inertial_orientation
+        tensor_orientation = torch.tensor(current_inertial_orientation)
+        inertial_forward = torch.inner(torch.stack([tensor_orientation.cos(), tensor_orientation.sin()]), inertial_position_difference)
+        self.last_inertial_position = current_inertial_position
+        self.last_inertial_orientation = current_inertial_orientation
+        for r in range(self.num_orientation_cells):
+            shifted = scipy.ndimage.shift(self.position_kernel, (0.0, inertial_forward/self.world_cell_size), output=None, order=3, mode='constant', cval=0.0, prefilter=True)
+            rotated = scipy.ndimage.rotate(shifted, -360 * r/self.num_orientation_cells, reshape=False)
+            self.current_probability_map[:,:,r] = torch.nn.functional.conv2d(self.current_probability_map[:,:,r:r+1].permute([2,0,1]), torch.tensor(rotated, dtype=torch.float).reshape([1,1,11,11]), padding="same").permute([1,2,0])[:,:,0]
+        #    shifted_orientation = scipy.ndimage.shift(self.orientation_kernel, inertial_orientation_difference * self.num_orientation_cells/ (2*3.141),
+        #                                 output=None, order=3, mode='wrap', cval=0.0, prefilter=True)
+
+        self.current_probability_map = torch.tensor(
+            scipy.ndimage.shift(self.current_probability_map, (0,0, inertial_orientation_difference * self.num_orientation_cells/ (2*3.141)), mode='wrap'),
+            dtype=torch.float)
+
+        #self.current_probability_map = torch.nn.functional.conv2d(
+        #    self.current_probability_map.permute([2,0,1]).reshape([1,self.num_orientation_cells, self.num_grid_cells, self.num_grid_cells]),
+        #    torch.tensor(shifted_orientation, dtype=torch.float).reshape([1, self.num_orientation_cells, 1, 1]))
+        # rescale
+        self.current_probability_map = torch.clip(self.current_probability_map, min=0.0)
+        self.current_probability_map = self.current_probability_map / self.current_probability_map.sum()
+        if inertial_position_difference.norm() > .001 or abs(inertial_orientation_difference) > .001:
+            print("Moving")
+        header = msg.header
+        self.publish_occupancy_grid_msg(self.current_probability_map, header)
+        self.publish_pose_msg(self.current_probability_map, header)
 
 
 def test1(node):
