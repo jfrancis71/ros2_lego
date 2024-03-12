6cf9a46df9bc20946c3fb30c87dd67577d7a3cd8
Julian Francis <julian.w.francis@gmail.com>
2024-03-12 14:11:43 +0000

    Adding robot description

--- nav/nav.py
+++ nav/nav.py
@@ -18,6 +18,9 @@ from cv_bridge import CvBridge
 from torchvision.utils import draw_bounding_boxes
 import inertial_nav as inertial_nav_mod
 import vision_nav as vision_nav_mod
+from tf2_ros.static_transform_broadcaster import StaticTransformBroadcaster
+from geometry_msgs.msg import TransformStamped
+
 
 Detection = collections.namedtuple("Detection", "label, bbox, score")
 
@@ -63,7 +66,8 @@ class Nav(Node):
         self.bridge = CvBridge()
         self.inertial_nav = inertial_nav_mod.InertialNav(self.num_grid_cells, self.num_orientation_cells, "Uniform")
         self.vision_nav = vision_nav_mod.VisionNav(self.num_grid_cells, self.num_orientation_cells)
-
+        self.tf_static_broadcaster = StaticTransformBroadcaster(self)
+        self.publish_occupancy_grid_msg(self.inertial_nav.current_probability_map, PoseStamped().header)
 
 
     def publish_occupancy_grid_msg(self, pose_probability_map, header):
@@ -72,7 +76,7 @@ class Nav(Node):
         conv = torch.nn.functional.conv2d(myprobs.unsqueeze(0), kernel, padding=1)[0]
         prob_map_msg = OccupancyGrid()
         prob_map_msg.header = header
-        prob_map_msg.header.frame_id = "base_link"
+        prob_map_msg.header.frame_id = "map"
         prob_map_msg.info.resolution = self.world_cell_size
         prob_map_msg.info.width = self.num_grid_cells
         prob_map_msg.info.height = self.num_grid_cells
@@ -87,7 +91,7 @@ class Nav(Node):
         orientation = pose_probability_map[loc[0], loc[1]].argmax()
         return (loc, orientation)
 
-    def publish_pose_msg(self, pose_probability_map, header):
+    def publish_pose_msg1(self, pose_probability_map, header):
         (loc, orientation) = self.get_location_MLE(pose_probability_map)
         pose_msg = Pose()
         point = Point()
@@ -100,8 +104,26 @@ class Nav(Node):
         pose_msg.orientation = q
         pose_stamped = PoseStamped()
         pose_stamped.header = header
+        pose_stamped.header.frame_id = "map"
         pose_stamped.pose = pose_msg
         self.pose_publisher.publish(pose_stamped)
+
+    def publish_pose_msg(self, pose_probability_map, header):
+        (loc, orientation) = self.get_location_MLE(pose_probability_map)
+        t = TransformStamped()
+        t.header.stamp = self.get_clock().now().to_msg()
+        t.header.frame_id = 'magnetic_north'
+        t.header = header
+        t.header.frame_id = "map"
+        t.child_frame_id = 'base_footprint'
+        quaternion = R.from_euler('xyz', [0, 0, (orientation/self.num_orientation_cells)*2*3.141]).as_quat()
+        q = Quaternion()
+        q.x, q.y, q.z, q.w = quaternion
+        t.transform.rotation = q
+        t.transform.translation.x = float(self.grid_cells_origin_x + loc[1]*self.world_cell_size)
+        t.transform.translation.y = float(self.grid_cells_origin_y + self.world_grid_length - (loc[0]*self.world_cell_size))
+        self.tf_static_broadcaster.sendTransform(t)
+        self.publish_pose_msg1(pose_probability_map, header)
 
     def annotated_image_callback(self, msg):
         cv_image = self.bridge.imgmsg_to_cv2(msg, desired_encoding="rgb8")
@@ -109,23 +131,34 @@ class Nav(Node):
         self.annotated_image = image
 
     def publish_vision_debug_image(self, pose_probability_map, header):
-        return
+#        return
         (loc, orientation) = self.get_location_MLE(pose_probability_map)
-        box_tensor = torch.tensor([self.dog_boxes.center.x[loc[0], loc[1], orientation]])
-        score = self.dog_boxes_probability[loc[0], loc[1], orientation]
-        box = Detection("debug", box_tensor, score)
-        center_x = self.dog_boxes.center.x[loc[0], loc[1], orientation]
-        center_y = self.dog_boxes.center.y[loc[0], loc[1], orientation]
-        width = self.dog_boxes.width[loc[0], loc[1], orientation]
-        height = self.dog_boxes.height[loc[0], loc[1], orientation]
-        xmin = center_x - width / 2
-        xmax = center_x + width / 2
-        ymin = center_y - height / 2
-        ymax = center_y + height / 2
-        box = torch.tensor([[xmin, ymin, xmax, ymax]])
-        print("Box=", box)
-        debug_image = draw_bounding_boxes(torch.tensor(self.annotated_image), box,
-                                              ["debug"], colors="green")
+        boxes = []
+        labels = []
+        for obj_label in self.vision_nav.object_list:
+            obj = self.vision_nav.object_dictionary[obj_label]
+            score = obj.detection_probabilities[loc[0], loc[1], orientation]
+            if score < .5:
+                continue
+#            box = Detection("debug", box_tensor, score)
+            center_x = obj.bounding_boxes.center.x[loc[0], loc[1], orientation]
+            center_y = obj.bounding_boxes.center.y[loc[0], loc[1], orientation]
+            width = obj.bounding_boxes.width[loc[0], loc[1], orientation]
+            height = obj.bounding_boxes.height[loc[0], loc[1], orientation]
+            xmin = center_x - width / 2
+            xmax = center_x + width / 2
+            ymin = center_y - height / 2
+            ymax = center_y + height / 2
+            box = torch.stack([xmin, ymin, xmax, ymax])
+            labels.append("debug_"+obj_label)
+            boxes.append(box)
+        if len(boxes) != 0:
+            tensor_boxes = torch.stack(boxes)
+            print("Box=", boxes)
+            debug_image = draw_bounding_boxes(torch.tensor(self.annotated_image), tensor_boxes,
+                                              labels, colors="green")
+        else:
+            debug_image = torch.tensor(self.annotated_image)
         ros2_image_msg = self.bridge.cv2_to_imgmsg(debug_image.numpy().transpose(1, 2, 0), encoding = "rgb8")
         ros2_image_msg.header = header
         self.debug_image_publisher.publish(ros2_image_msg)
@@ -154,10 +187,11 @@ class Nav(Node):
             self.state = 0
         else:
             if self.state == 0:
-                self.publish_vision_debug_image(pose_from_detections_probability_map, msg.header)
+
                 self.inertial_nav.update_from_sensor(pose_from_detections_probability_map)
                 self.state = 1
         header = msg.header
+        self.publish_vision_debug_image(pose_from_detections_probability_map, msg.header)
         self.publish_occupancy_grid_msg(self.inertial_nav.current_probability_map, header)
         self.publish_pose_msg(self.inertial_nav.current_probability_map, header)
 
