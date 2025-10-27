e5d047a6a07a650ad65cd93aa1f76c349447819c
Julian Francis <julian.w.francis@gmail.com>
2025-10-27 15:31:23 +0000

    Refactoring...

--- lidar_nav/localizer.py
+++ lidar_nav/localizer.py
@@ -16,6 +16,28 @@ from tf2_ros.transform_listener import TransformListener
 from tf2_ros import TransformException
 
 
+class DiscreteLocalizer:
+    def __init__(self, map_image, origin, resolution, num_degrees, num_x_resolution, num_y_resolution):
+        self.num_degrees = num_degrees
+        self.resolution = resolution
+        self.origin = origin
+        self.centers = [[(r,c) for c in range(0, map_image.shape[1], int(num_x_resolution/resolution))] for r in range(0, map_image.shape[0], int(num_y_resolution/resolution))]
+        self.centers = list(itertools.chain(*self.centers))
+        self.trans = np.array([skimage.transform.warp_polar(map_image==0, center=c, radius=100, output_shape=(num_degrees, 100)) for c in self.centers])
+        self.polar_coords = np.array([np.argmax(self.trans[c], axis=1)*resolution for c in range(len(self.centers))])
+        self.map_height = map_image.shape[0]
+
+    def localize(self, scan):
+        new_scan = skimage.transform.resize(scan.astype(np.float32), (self.num_degrees,))
+        predictions = np.array([[np.roll(np.flip(self.polar_coords[c]), s) for s in range(self.num_degrees)] for c in range(len(self.centers))])
+
+        angles = np.nanmean((predictions - new_scan)**2, axis=0)[np.newaxis, np.newaxis]
+        prediction_error = np.nanmean((predictions - new_scan)**2, axis=2)
+        idx = np.unravel_index(np.argmin(prediction_error), prediction_error.shape)
+        loc = idx[0]
+        angle = 2 * np.pi * idx[1]/self.num_degrees
+        return (self.centers[loc][1]*self.resolution + self.origin[0], (self.map_height-self.centers[loc][0])*self.resolution + self.origin[1]), angle, predictions[loc][idx[1]]
+
 class Localizer(Node):
     def __init__(self):
         super().__init__("nav")
@@ -24,15 +46,15 @@ class Localizer(Node):
         self.declare_parameter('discrete_num_x_resolution', .05)
         self.declare_parameter('discrete_num_y_resolution', .05)
         self.map_file = self.get_parameter('map').get_parameter_value().string_value
-        self.num_degrees = self.get_parameter('discrete_num_degrees').get_parameter_value().integer_value
-        self.discrete_num_x_resolution = self.get_parameter('discrete_num_x_resolution').get_parameter_value().double_value
-        self.discrete_num_y_resolution = self.get_parameter('discrete_num_x_resolution').get_parameter_value().double_value
+        self.discrete_num_degrees = self.get_parameter('discrete_num_degrees').get_parameter_value().integer_value
+        discrete_num_x_resolution = self.get_parameter('discrete_num_x_resolution').get_parameter_value().double_value
+        discrete_num_y_resolution = self.get_parameter('discrete_num_x_resolution').get_parameter_value().double_value
 
         with open(self.map_file, 'r') as map_file:
             map_properties = yaml.safe_load(map_file)
-            self.image_filename = map_properties['image']
-            self.origin = map_properties['origin']
-            self.resolution = map_properties["resolution"]
+            image_filename = map_properties['image']
+            origin = map_properties['origin']
+            resolution = map_properties["resolution"]
         self.lidar_subscription = self.create_subscription(
             LaserScan,
             "/scan",
@@ -43,11 +65,8 @@ class Localizer(Node):
         self.tf_buffer = Buffer()
         self.tf_listener = TransformListener(self.tf_buffer, self)
         self.tf_broadcaster = TransformBroadcaster(self)
-        self.map = skimage.io.imread(os.path.join(os.path.split(self.map_file)[0], self.image_filename))
-        self.centers = [[(r,c) for c in range(0, self.map.shape[1], int(self.discrete_num_x_resolution/self.resolution))] for r in range(0, self.map.shape[0], int(self.discrete_num_y_resolution/self.resolution))]
-        self.centers = list(itertools.chain(*self.centers))
-        self.trans = np.array([skimage.transform.warp_polar(self.map==0, center=c, radius=100, output_shape=(self.num_degrees, 100)) for c in self.centers])
-        self.polar_coords = np.array([np.argmax(self.trans[c], axis=1)*self.resolution for c in range(len(self.centers))])
+        map = skimage.io.imread(os.path.join(os.path.split(self.map_file)[0], image_filename))
+        self.localizer = DiscreteLocalizer(map, origin, resolution, self.discrete_num_degrees, discrete_num_x_resolution, discrete_num_y_resolution)
 
     def send_map_base_link_transform(self, loc, angle, tim):
         try:
@@ -88,9 +107,9 @@ class Localizer(Node):
         map_to_map_base_laser_transform.header.frame_id = 'map'
         map_to_map_base_laser_transform.child_frame_id = 'map_base_laser'
         map_to_map_base_laser_transform.transform.translation.x = \
-            self.centers[loc][1]*self.resolution + self.origin[0]
+            loc[0]
         map_to_map_base_laser_transform.transform.translation.y = \
-            (self.map.shape[0]-self.centers[loc][0])*self.resolution + self.origin[1]
+            loc[1]
         map_to_map_base_laser_transform.transform.translation.z = 0.0
         q = quaternion_from_euler(0, 0, -angle)
         map_to_map_base_laser_transform.transform.rotation.x = q[0]
@@ -105,7 +124,7 @@ class Localizer(Node):
         lidar_msg1.ranges = ranges
         lidar_msg1.angle_min = 0.0
         lidar_msg1.angle_max = 6.28318548
-        lidar_msg1.angle_increment = 2 * np.pi / 360.
+        lidar_msg1.angle_increment = 2 * np.pi / self.discrete_num_degrees
         lidar_msg1.time_increment = 0.00019850002718158066
         lidar_msg1.scan_time = 0.10004401206970215
         lidar_msg1.range_min = 0.019999999552965164
@@ -115,16 +134,9 @@ class Localizer(Node):
 
     def lidar_callback(self, lidar_msg):
         scan = np.array(lidar_msg.ranges)
-        new_scan = skimage.transform.resize(scan.astype(np.float32), (self.num_degrees,))
-        predictions = np.array([[np.roll(np.flip(self.polar_coords[c]), s) for s in range(359)] for c in range(len(self.centers))])
-
-        angles = np.nanmean((predictions - new_scan)**2, axis=0)[np.newaxis, np.newaxis]
-        prediction_error = np.nanmean((predictions - new_scan)**2, axis=2)
-        idx = np.unravel_index(np.argmin(prediction_error), prediction_error.shape)
-        loc = idx[0]
-        angle = 2 * np.pi * idx[1]/360.0
+        loc, angle, predictions = self.localizer.localize(scan)
         self.send_map_base_link_transform(loc, angle, None)
-        self.publish_lidar_prediction(lidar_msg.header, predictions[loc][idx[1]])
+        self.publish_lidar_prediction(lidar_msg.header, predictions)
 
 
 rclpy.init()
