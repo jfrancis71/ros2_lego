e8f00f52b9fb1cceb5258cf7269f5f80f7913b5b
Julian Francis <julian.w.francis@gmail.com>
2025-11-08 09:54:53 +0000

    Adding service.

--- repub/repub.py
+++ repub/repub.py
@@ -6,6 +6,7 @@ from cv_bridge import CvBridge
 import cv2
 import numpy as np
 import yaml
+from sensor_msgs.srv import SetCameraInfo
 
 
 class StereoSplitNode(Node):
@@ -24,33 +25,28 @@ class StereoSplitNode(Node):
                 self.create_publisher(CameraInfo, "/stereo/left/camera_info", 1)
         self.right_cam_info_publisher = \
                 self.create_publisher(CameraInfo, "/stereo/right/camera_info", 1)
+        self.left_srv = self.create_service(SetCameraInfo, '/stereo/left_camera/set_camera_info', self.set_left_camera_info_callback)
+        self.right_srv = self.create_service(SetCameraInfo, '/stereo/right_camera/set_camera_info', self.set_right_camera_info_callback)
+        #self.left_srv = self.create_service(SetCameraInfo, 'left_camera/set_camera_info', self.set_left_camera_info_callback)
+        #self.right_srv = self.create_service(SetCameraInfo, 'right_camera/set_camera_info', self.set_right_camera_info_callback)
         self.bridge = CvBridge()
-        try:
-            with open("/root/ros2_ws/calib/left.yaml", 'r') as stream:
-                left_cam_data = yaml.safe_load(stream)
-                self.left_cam_info = CameraInfo()
-                self.left_cam_info.width = 320
-                self.left_cam_info.height = 240
-                self.left_cam_info.distortion_model = 'plumb_bob'
-                self.left_cam_info.d = left_cam_data["distortion_coefficients"]["data"]
-                self.left_cam_info.r = left_cam_data["rectification_matrix"]["data"]
-                self.left_cam_info.p = left_cam_data["projection_matrix"]["data"]
-                self.left_cam_info.k = left_cam_data["camera_matrix"]["data"]
-        except FileNotFoundError:
-            self.left_cam_info = None
-        try:
-            with open("/root/ros2_ws/calib/right.yaml", 'r') as stream:
-                right_cam_data = yaml.safe_load(stream)
-                self.right_cam_info = CameraInfo()
-                self.right_cam_info.width = 320
-                self.right_cam_info.height = 240
-                self.right_cam_info.distortion_model = 'plumb_bob'
-                self.right_cam_info.d = right_cam_data["distortion_coefficients"]["data"]
-                self.right_cam_info.r = right_cam_data["rectification_matrix"]["data"]
-                self.right_cam_info.p = right_cam_data["projection_matrix"]["data"]
-                self.right_cam_info.k = right_cam_data["camera_matrix"]["data"]
-        except FileNotFoundError:
-            self.right_cam_info = None
+        self.bridge = CvBridge()
+        self.left_cam_info = None
+        self.right_cam_info = None
+
+    def set_left_camera_info_callback(self, request, response):
+        print("Hello received left service request.")
+        self.left_cam_info = request.camera_info
+        response.success = True
+        response.status_message = ""
+        return response
+
+    def set_right_camera_info_callback(self, request, response):
+        print("Hello received right service request.")
+        self.right_cam_info = request.camera_info
+        response.success = True
+        response.status_message = ""
+        return response
 
     def image_callback(self, image_msg):
         cv_image = self.bridge.imgmsg_to_cv2(image_msg, desired_encoding='bgr8')
@@ -63,8 +59,8 @@ class StereoSplitNode(Node):
         if self.left_cam_info is not None and self.right_cam_info is not None:
             self.left_cam_info.header = image_msg.header
             self.right_cam_info.header = image_msg.header
-            self.left_cam_info_publisher.publish(self.left_cam)
-            self.right_cam_info_publisher.publish(self.right_cam)
+            self.left_cam_info_publisher.publish(self.left_cam_info)
+            self.right_cam_info_publisher.publish(self.right_cam_info)
 
 
 rclpy.init()
