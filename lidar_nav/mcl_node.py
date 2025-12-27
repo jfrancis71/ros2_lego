f5a48390346ff034a64ae5e72f8cfbaf08b8e241
Julian Francis <julian.w.francis@gmail.com>
2025-12-27 14:51:35 +0000

    Improved TF naming.

--- lidar_nav/mcl_node.py
+++ lidar_nav/mcl_node.py
@@ -150,8 +150,8 @@ class MCLNode(Node):
             resolution = map_properties["resolution"]
         map = skimage.io.imread(os.path.join(os.path.split(self.map_file)[0], image_filename))
         self.localizer = MCL(map, origin, resolution)
-        self.old_transform = None
-        self.last_lidar_update_transform = None
+        self.tf_previous_odom = None
+        self.tf_previous_lidar_update = None
         self.tf_static_broadcaster = StaticTransformBroadcaster(self)
         self.initial_pose_received = False
         self.diff_t = 0.03
@@ -292,46 +292,46 @@ class MCLNode(Node):
             return
         scan = np.array(lidar_msg.ranges)
         try:
-            base_laser_to_odom_transform = self.tf_buffer.lookup_transform(
+            tf_base_laser_to_odom = self.tf_buffer.lookup_transform(
                 "base_laser",
                 "odom",
                 rclpy.time.Time())
-            odom_to_base_laser_transform = self.tf_buffer.lookup_transform(
+            tf_odom_to_base_laser = self.tf_buffer.lookup_transform(
                 "odom",
                 "base_laser",
                 rclpy.time.Time())
         except TransformException as ex:
             print("No base laser to odom Transform")
             return
-        if self.old_transform is None:
-            self.old_transform = odom_to_base_laser_transform
-            self.last_lidar_update_transform = odom_to_base_laser_transform
+        if self.tf_previous_odom is None:
+            self.tf_previous_odom = tf_odom_to_base_laser
+            self.tf_previous_lidar_update = tf_odom_to_base_laser
         lidar_msg_time = Time.from_msg(lidar_msg.header.stamp)
-        odom_base_tf_time = Time.from_msg(odom_to_base_laser_transform.header.stamp)
+        odom_base_tf_time = Time.from_msg(tf_odom_to_base_laser.header.stamp)
         delay = (lidar_msg_time-odom_base_tf_time).nanoseconds*1e-9
         if delay > .1:
             print("DELAY ", delay)
             return
-        old_odom_pose = self.ros2_to_pose(self.old_transform)
-        new_odom_pose = self.ros2_to_pose(odom_to_base_laser_transform)
-        self.localizer.update_particles_odom(old_odom_pose, new_odom_pose)
-        last_lidar_update_pose = self.ros2_to_pose(self.last_lidar_update_transform)
-        diff_x = new_odom_pose[0] - last_lidar_update_pose[0]
-        diff_y = new_odom_pose[1] - last_lidar_update_pose[1]
+        previous_odom_pose = self.ros2_to_pose(self.tf_previous_odom)
+        current_odom_pose = self.ros2_to_pose(tf_odom_to_base_laser)
+        self.localizer.update_particles_odom(previous_odom_pose, current_odom_pose)
+        previous_lidar_update_pose = self.ros2_to_pose(self.tf_previous_lidar_update)
+        diff_x = current_odom_pose[0] - previous_lidar_update_pose[0]
+        diff_y = current_odom_pose[1] - previous_lidar_update_pose[1]
         d_trans = np.sqrt(diff_y**2 + diff_x**2)
-        abs_diff_angle = np.abs(new_odom_pose[2] - last_lidar_update_pose[2])
+        abs_diff_angle = np.abs(current_odom_pose[2] - previous_lidar_update_pose[2])
         diff_angle = np.min(np.array([abs_diff_angle, 2*np.pi - abs_diff_angle]))
         abs_diff = np.abs(diff_angle)
         if abs_diff > self.diff_angle or d_trans > self.diff_t:
             self.localizer.update_particles_lidar(scan)
-            self.last_lidar_update_transform = odom_to_base_laser_transform
-        time.sleep(.2)  # This helps with "future transform" error. Why?
+            self.tf_last_lidar_update = tf_odom_to_base_laser
+        time.sleep(.3)  # This helps with "future transform" error. Why?
         pose, pose_uncertainty = self.localizer.expected_pose(self.localizer.particles[:self.localizer.replacement])
         mean_predictions = self.localizer.range_predictions(np.array([[pose[0], pose[1], pose[2]]]))
         new_scan = skimage.transform.resize(scan.astype(np.float32), (self.localizer.num_angles,))
         logprob_ranges = self.localizer.logprob_range_prediction(mean_predictions, new_scan[np.newaxis, :])
-        self.publish_ros2(lidar_msg.header, base_laser_to_odom_transform, pose, pose_uncertainty, self.localizer.particles, mean_predictions[0], logprob_ranges[0])
-        self.old_transform = odom_to_base_laser_transform
+        self.publish_ros2(lidar_msg.header, tf_base_laser_to_odom, pose, pose_uncertainty, self.localizer.particles, mean_predictions[0], logprob_ranges[0])
+        self.tf_previous_odom = tf_odom_to_base_laser
 
 
 rclpy.init()
