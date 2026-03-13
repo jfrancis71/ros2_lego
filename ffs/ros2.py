4600a536a2e64fa5354b1f9e9f822701edf993b2
Julian Francis <julian.w.francis@gmail.com>
2026-03-13 16:39:21 +0000

    init revision. very messy. sort of works though...

---
+++ ffs/ros2.py
@@ -0,0 +1,136 @@
+import rclpy
+from rclpy.node import Node
+from sensor_msgs.msg import Image, PointCloud2, PointField
+from cv_bridge import CvBridge
+import numpy as np
+import torch
+import cv2
+
+import struct
+
+from Utils import (
+    AMP_DTYPE, set_logging_format, set_seed, vis_disparity,
+    depth2xyzmap, toOpen3dCloud, o3d,
+)
+
+from core.utils.utils import InputPadder
+
+
+class FFSNode(Node):
+    def __init__(self):
+        super().__init__("ffs_node")
+        self.subscription = self.create_subscription(
+            Image,
+            "/left/image_rect_color",
+            self.left_image_callback,
+            1)
+        self.subscription = self.create_subscription(
+            Image,
+            "/right/image_rect_color",
+            self.right_image_callback,
+            1)
+        self.image_publisher = \
+                self.create_publisher(Image, "depth_image", 1)
+        self.pcd_publisher = self.create_publisher(PointCloud2, 'ffs_pcd', 1)
+        self.bridge = CvBridge()
+        self.get_logger().info("Node has started.")
+        self.model = torch.load("/root/Fast-FoundationStereo/weights/20-30-48/model_best_bp2_serialize.pth", map_location='cpu', weights_only=False)
+        self.model.cuda().eval()
+        self.left_image = None
+
+    def convert(self, msg):
+        cv_image = self.bridge.imgmsg_to_cv2(msg, desired_encoding="rgb8")
+        image = cv_image.copy().transpose((2, 0, 1))
+        return image
+
+    def left_image_callback(self, msg):
+        self.left_image = self.convert(msg)
+
+    def right_image_callback(self, msg):
+        if self.left_image is None:
+            return
+        right_image = self.convert(msg)
+        left_batch_image = np.expand_dims(self.left_image, axis=0)
+        left_tensor_image = torch.tensor(left_batch_image, dtype=torch.float32, device="cuda")
+        right_batch_image = np.expand_dims(right_image, axis=0)
+        right_tensor_image = torch.tensor(right_batch_image, dtype=torch.float32, device="cuda")
+
+        padder = InputPadder(left_tensor_image.shape, divis_by=32, force_square=False)
+        img0, img1 = padder.pad(left_tensor_image, right_tensor_image)
+
+        with torch.amp.autocast('cuda', enabled=True, dtype=AMP_DTYPE):
+            disp = self.model.forward(img0, img1, iters=4, test_mode=True, optimize_build_volume='pytorch1')
+#            disp = torch.clamp(disp*3, 0.0, 255.0)
+            tens = torch.tensor(disp, dtype=torch.uint8)
+            print("DI=", disp.shape)
+        disp = padder.unpad(disp.float())
+        H, W = 240, 320
+        disp = disp.data.cpu().numpy().reshape(H,W).clip(0, None)
+
+        cmap = None
+        min_val = None
+        max_val = None
+        vis = vis_disparity(disp, min_val=min_val, max_val=max_val, cmap=cmap, color_map=cv2.COLORMAP_TURBO)
+        left_orig = self.left_image.copy().transpose((1, 2, 0))
+        right_orig = right_image.copy().transpose((1, 2, 0))
+        print("VIS SHAPE=", vis.shape, "LEFT=", left_orig.shape, "RIGHT=", right_orig.shape)
+        vis = np.concatenate([left_orig, right_orig, vis], axis=1)
+        s = 1280/vis.shape[1]
+        resized_vis = cv2.resize(vis, (int(vis.shape[1]*s), int(vis.shape[0]*s)))
+
+        ros2_image_msg = self.bridge.cv2_to_imgmsg(resized_vis,
+                                                   encoding="rgb8")
+        ros2_image_msg.header = msg.header
+        self.image_publisher.publish(ros2_image_msg)
+
+        K = np.array([130.39136801932622, 0.0, 153.42, 0.0, 130.09, 115.67, 0.0, 0.0, 1.0]).reshape(3,3)
+        baseline = .062
+        depth = K[0,0]*baseline/disp
+        xyz_map = depth2xyzmap(depth, K)
+        pcd = toOpen3dCloud(xyz_map.reshape(-1,3), left_orig.reshape(-1,3))
+        ros_dtype = PointField.FLOAT32
+
+        points = np.array(pcd.points)
+        colors = np.array(pcd.colors)
+
+        itemsize = np.dtype(np.float32).itemsize
+
+        fields = [
+            PointField(name='x', offset=0, datatype=PointField.FLOAT32, count=1),
+            PointField(name='y', offset=4, datatype=PointField.FLOAT32, count=1),
+            PointField(name='z', offset=8, datatype=PointField.FLOAT32, count=1),
+            PointField(name='rgb', offset=12, datatype=PointField.UINT32, count=1),
+        ]
+
+        point_struct = struct.Struct("<fffBBB")
+        num_points = points.shape[0]
+        buffer = bytearray(point_struct.size * num_points)
+        for i in range(num_points):
+            r, g, b = colors[i]
+            r, g, b = colors[i, 0]*255, colors[i, 1]*255, colors[i, 2]*255
+            r, g, b = int(r), int(g), int(b)
+            point_struct.pack_into(
+                buffer, i * point_struct.size, points[i,0], points[i,1], points[i,2], b, g, r
+        )
+
+        pcd_msg = PointCloud2(
+            header=msg.header,
+            height=1,
+            width=points.shape[0],
+            is_dense=False,
+            is_bigendian=False,
+            fields=fields,
+            point_step=15,
+            row_step=(itemsize * 3 * points.shape[0]),
+            data=buffer
+        )
+        self.pcd_publisher.publish(pcd_msg)
+
+
+torch.autograd.set_grad_enabled(False)
+
+rclpy.init()
+ffs_node = FFSNode()
+rclpy.spin(ffs_node)
+ffs_node.destroy_node()
+rclpy.shutdown()
