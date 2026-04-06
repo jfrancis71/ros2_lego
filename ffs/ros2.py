25e5343bfcd8b718fde6a16b9a3a3e8279fbd038
Julian Francis <julian.w.francis@gmail.com>
2026-04-06 15:08:52 +0000

    Code tidy up with refactoring...

--- ffs/ros2.py
+++ ffs/ros2.py
@@ -27,64 +27,39 @@ class FFSNode(Node):
         self.left_image_info_sub = Subscriber(self, CameraInfo, "/left/camera_info")
         self.right_image_info_sub = Subscriber(self, CameraInfo, "/right/camera_info")
         self.image_publisher = \
-                self.create_publisher(Image, "depth_image", 1)
-        self.pcd_publisher = self.create_publisher(PointCloud2, 'ffs_pcd', 1)
+                self.create_publisher(Image, "/ffs_disparity", 1)
+        self.pcd_publisher = self.create_publisher(PointCloud2, '/ffs_points2', 1)
+        queue_size = 1
+        self.sync = TimeSynchronizer([self.left_image_sub, self.right_image_sub, self.left_image_info_sub, self.right_image_info_sub], queue_size)
+        self.sync.registerCallback(self.sync_cb)
         self.bridge = CvBridge()
         self.model = torch.load("/root/Fast-FoundationStereo/weights/20-30-48/model_best_bp2_serialize.pth", map_location='cpu', weights_only=False)
         self.model.cuda().eval()
-        queue_size = 10
-        self.sync = TimeSynchronizer([self.left_image_sub, self.right_image_sub, self.left_image_info_sub, self.right_image_info_sub], queue_size)
-        self.sync.registerCallback(self.SyncCallback)
         self.get_logger().info("Node has started.")
 
-    def convert(self, msg):
-        cv_image = self.bridge.imgmsg_to_cv2(msg, desired_encoding="rgb8")
-        image = cv_image.copy().transpose((2, 0, 1))
-        return image
+    def move_image_to_gpu(self, image):
+        image = image.copy().transpose((2, 0, 1))
+        batch_image = np.expand_dims(image, axis=0)
+        return torch.tensor(batch_image, dtype=torch.float32, device="cuda")
 
-    def SyncCallback(self, left_msg, right_msg, left_info_msg, right_info_msg):
-        left_image = self.convert(left_msg)
-        right_image = self.convert(right_msg)
-        left_batch_image = np.expand_dims(left_image, axis=0)
-        left_tensor_image = torch.tensor(left_batch_image, dtype=torch.float32, device="cuda")
-        right_batch_image = np.expand_dims(right_image, axis=0)
-        right_tensor_image = torch.tensor(right_batch_image, dtype=torch.float32, device="cuda")
-
-        padder = InputPadder(left_tensor_image.shape, divis_by=32, force_square=False)
-        img0, img1 = padder.pad(left_tensor_image, right_tensor_image)
-
-        with torch.amp.autocast('cuda', enabled=True, dtype=AMP_DTYPE):
-            disp = self.model.forward(img0, img1, iters=4, test_mode=True, optimize_build_volume='pytorch1')
-            tens = torch.tensor(disp, dtype=torch.uint8)
-        disp = padder.unpad(disp.float())
-        disp = disp.data.cpu().numpy().reshape(left_info_msg.height,left_info_msg.width).clip(0, None)
-
-        cmap = None
-        min_val = None
-        max_val = None
-        vis = vis_disparity(disp, min_val=min_val, max_val=max_val, cmap=cmap, color_map=cv2.COLORMAP_TURBO)
-        left_orig = left_image.copy().transpose((1, 2, 0))
-        right_orig = right_image.copy().transpose((1, 2, 0))
-        vis = np.concatenate([left_orig, right_orig, vis], axis=1)
-        s = 1280/vis.shape[1]
-        resized_vis = cv2.resize(vis, (int(vis.shape[1]*s), int(vis.shape[0]*s)))
-
-        ros2_image_msg = self.bridge.cv2_to_imgmsg(resized_vis,
+    def publish_disparity_image(self, disparity_map, header):
+        vis = vis_disparity(disparity_map, min_val=None, max_val=None, cmap=None, color_map=cv2.COLORMAP_TURBO)
+        ros2_image_msg = self.bridge.cv2_to_imgmsg(vis,
                                                    encoding="rgb8")
-        ros2_image_msg.header = left_msg.header
+        ros2_image_msg.header = header
         self.image_publisher.publish(ros2_image_msg)
 
+    def publish_point_cloud(self, disparity_map, left_img,
+            left_info_msg, right_info_msg, header):
         K = left_info_msg.k.reshape(3,3)
         baseline = -right_info_msg.p[3] / right_info_msg.p[0]
-        depth = K[0,0]*baseline/disp
+        depth = K[0,0]*baseline/disparity_map
         xyz_map = depth2xyzmap(depth, K)
-        pcd = toOpen3dCloud(xyz_map.reshape(-1,3), left_orig.reshape(-1,3))
+        pcd = toOpen3dCloud(xyz_map.reshape(-1,3), left_img.reshape(-1,3))
         ros_dtype = PointField.FLOAT32
-
         points = np.array(pcd.points, dtype=np.float32)
         colors = np.array(np.array(pcd.colors, dtype=np.float32)*255, dtype=np.uint8)
         colors = np.flip(colors, axis=1)
-
         itemsize = np.dtype(np.float32).itemsize
         fields = [
             PointField(name='x', offset=0, datatype=PointField.FLOAT32, count=1),
@@ -97,7 +72,7 @@ class FFSNode(Node):
         byte_array[:,:12] = np.frombuffer(points.tobytes(), dtype=np.byte).reshape(num_points, 3*4)
         byte_array[:, 12:] = np.frombuffer( colors.tobytes(), dtype=np.byte).reshape(num_points, 3)
         pcd_msg = PointCloud2(
-            header=left_msg.header,
+            header=header,
             height=1,
             width=points.shape[0],
             is_dense=False,
@@ -109,9 +84,25 @@ class FFSNode(Node):
         )
         self.pcd_publisher.publish(pcd_msg)
 
+    def sync_cb(self, left_img_msg, right_img_msg, left_info_msg, right_info_msg):
+        left_img = self.bridge.imgmsg_to_cv2(left_img_msg, desired_encoding="rgb8")
+        right_img = self.bridge.imgmsg_to_cv2(right_img_msg, desired_encoding="rgb8")
+        left_tensor_img = self.move_image_to_gpu(left_img)
+        right_tensor_img = self.move_image_to_gpu(right_img)
+        padder = InputPadder(left_tensor_img.shape, divis_by=32, force_square=False)
+        pad_left_img, pad_right_img = padder.pad(left_tensor_img, right_tensor_img)
+        with torch.amp.autocast('cuda', enabled=True, dtype=AMP_DTYPE):
+            gpu_padded_disparity_map = self.model.forward(
+                pad_left_img, pad_right_img, iters=4, test_mode=True,
+                optimize_build_volume='pytorch1')
+        gpu_disparity_map = padder.unpad(gpu_padded_disparity_map.float())[0,0]
+        disparity_map = gpu_disparity_map.data.cpu().numpy().clip(0, None)
+        self.publish_disparity_image(disparity_map, left_img_msg.header)
+        self.publish_point_cloud(disparity_map, left_img,
+            left_info_msg, right_info_msg, left_img_msg.header)
+
 
 torch.autograd.set_grad_enabled(False)
-
 rclpy.init()
 ffs_node = FFSNode()
 rclpy.spin(ffs_node)
