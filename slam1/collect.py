6940446c9c6175ef45f8a2a53f9d1cf3f44fd290
Julian Francis <julian.w.francis@gmail.com>
2026-08-05 13:51:55 +0000

    init revision.

---
+++ slam1/collect.py
@@ -0,0 +1,207 @@
+import copy
+import os
+import yaml
+import numpy as np
+#import scipy
+from scipy import ndimage as ndi
+from scipy.stats import uniform, norm, vonmises
+from scipy.spatial.transform import Rotation as R
+import scipy.stats as stats
+import skimage
+import rclpy
+from rclpy.node import Node
+from rclpy.time import Time
+from rclpy.qos import QoSProfile, DurabilityPolicy, HistoryPolicy
+from sensor_msgs.msg import LaserScan
+from visualization_msgs.msg import Marker
+from geometry_msgs.msg import Point, TransformStamped, PoseWithCovarianceStamped
+from tf2_ros.transform_listener import TransformListener
+# Current StaticTransformBroadcaster is broken, we need to use from rolling.
+# clone git clone https://github.com/ros2/geometry2.git
+# Prepend ./src/geometry2/tf2_ros_py/tf2_ros to PYTHONPATH and export
+from static_transform_broadcaster import StaticTransformBroadcaster
+from tf2_ros import TransformException
+from tf_transformations import euler_from_quaternion, quaternion_from_euler
+from tf2_ros.buffer import Buffer
+import time
+import message_filters
+from nav_msgs.msg import OccupancyGrid
+
+
+def angle_diff(angle_1, angle_2):
+    """Returns closest difference between two angles.
+
+       Note you cannot just subtract the angles, eg 3.1 - (-3.1) = 6.2. This not the closest angle change.
+
+    """
+    abs_diff_angle = np.abs(angle_1 - angle_2)
+    return np.min(np.array([abs_diff_angle, 2*np.pi - abs_diff_angle]))
+
+
+class SLAM:
+    def __init__(self):
+        self.num_particles = 240
+        self.num_angles = 360
+        self.particles = np.tile(np.array([0.0, 0.0, -0.5 * np.pi]), reps=(self.num_particles, 1))
+        # shape N, P where N is particle no, T is time, P is pose shape
+        self.scans = []
+        self.odom_poses = []
+
+    def init(self, raw_scan, current_odom_pose):
+        self.scans.append(np.array(skimage.transform.resize(raw_scan.astype(np.float32), (self.num_angles,))))
+        self.odom_poses.append(current_odom_pose)
+
+
+    def update(self, scan, previous_odom_pose, current_odom_pose):
+        self.particles = self.extend(previous_odom_pose, current_odom_pose)
+
+    def extend(self, previous_odom_pose, current_odom_pose):
+        alpha1 = 0.15
+        alpha3 = 0.05
+        diff_x = current_odom_pose[0] - previous_odom_pose[0]
+        diff_y = current_odom_pose[1] - previous_odom_pose[1]
+        d_rot1 = np.arctan2(diff_y, diff_x) - previous_odom_pose[2]
+        d_trans = np.sqrt(diff_y**2 + diff_x**2)
+        d_rot2 = current_odom_pose[2] - previous_odom_pose[2] - d_rot1
+        diff_angle = angle_diff(current_odom_pose[2], previous_odom_pose[2])
+        sp1 = np.random.normal(size=self.num_particles)
+        sp2 = np.random.normal(size=self.num_particles)
+        sp3 = np.random.normal(size=self.num_particles)
+        sample_d_rot1 = d_rot1 + sp1*diff_angle *alpha1
+        sample_d_trans = d_trans + sp2*d_trans* alpha3
+        sample_d_rot2 = d_rot2 + sp3*diff_angle *alpha1
+        particles = np.zeros([self.num_particles, 3])
+        particles[:, 0] = self.particles[:, 0] + sample_d_trans * np.cos(self.particles[:, 2] + sample_d_rot1)
+        particles[:, 1] = self.particles[:, 1] + sample_d_trans * np.sin(self.particles[:, 2] + sample_d_rot1)
+        particles[:,  2] = self.particles[:, 2] + sample_d_rot1 + sample_d_rot2
+        return particles
+
+    def expected_pose(self):
+        x_mean, y_mean, _ = np.mean(self.particles, axis=0)
+        _, angle, _ = vonmises.fit(self.particles[:, 2], fscale=1)
+        return x_mean, y_mean, angle
+
+
+class SLAMNode(Node):
+    def __init__(self):
+        super().__init__("slam_node")
+        self.min_dist = 0.03  # minimum distance for lidar update
+        self.min_angle = .08  # minimum angle change for lidar update
+        self.lidar_subscription = self.create_subscription(
+            LaserScan,
+            "/scan",
+            self.lidar_callback,
+            1)
+
+        self.tf_buffer = Buffer()
+        qos = QoSProfile(
+            depth=100,
+            durability=DurabilityPolicy.VOLATILE,
+            history=HistoryPolicy.KEEP_LAST,
+            )
+        self.tf_listener = TransformListener(self.tf_buffer, self, spin_thread=True, qos=qos)
+        self.current_lidar_msg = None
+        self.previous_odom_pose = None
+        self.tf_static_broadcaster = StaticTransformBroadcaster(self)
+        self.init_wait = 0
+        self.marker_pdf_publisher = self.create_publisher(Marker, '/particles_marker'
+, 1)
+
+        self.slam = SLAM()
+
+    def publish_map_odom_transform(self, tf_base_laser_to_odom, pose):
+        tf_zero_to_odom = TransformStamped()
+        tf_zero_to_odom.header.stamp = self.current_lidar_msg.header.stamp
+        tf_zero_to_odom.header.frame_id = 'zero'
+        tf_zero_to_odom.child_frame_id = 'odom'
+        tf_zero_to_odom.transform = tf_base_laser_to_odom.transform
+        tf_map_to_zero = TransformStamped()
+        tf_map_to_zero.header.stamp = self.current_lidar_msg.header.stamp
+        tf_map_to_zero.header.frame_id = 'map'
+        tf_map_to_zero.child_frame_id = 'zero'
+        tf_m_to_z_trans = tf_map_to_zero.transform.translation
+        tf_m_to_z_trans.x, tf_m_to_z_trans.y, tf_m_to_z_trans.z = pose[0], pose[1], 0.0
+        q = quaternion_from_euler(0, 0, pose[2])
+        tf_m_to_z_rot = tf_map_to_zero.transform.rotation
+        tf_m_to_z_rot.x, tf_m_to_z_rot.y, tf_m_to_z_rot.z, tf_m_to_z_rot.w = q[0], q[1], q[2], q[3]
+        self.tf_static_broadcaster.sendTransform([tf_zero_to_odom, tf_map_to_zero])
+
+    def publish_particles(self, pose):
+        marker = Marker()
+        marker.header.stamp = self.current_lidar_msg.header.stamp
+        marker.header.frame_id = "base_laser"
+        marker.ns = "basic_shapes"
+        marker.id = 0
+        marker.type = Marker.POINTS
+        marker.action = Marker.ADD
+        marker.pose.position.x, marker.pose.position.y, marker.pose.position.z = 0.0, 0.0, 0.0
+        marker.pose.orientation.x, marker.pose.orientation.y, marker.pose.orientation.z = 0.0, 0.0, 0.0
+        marker.pose.orientation.w = 1.0
+        marker.scale.x, marker.scale.y, marker.scale.z = 0.03, 0.03, 0.05
+        marker.color.r, marker.color.g, marker.color.b, marker.color.a = 0.3, 1.0, 1.0, .2
+        particles_base_laser = np.matmul(self.slam.particles[:, :2] - pose[:2], R.from_rotvec([0, 0, -pose[2]]).as_matrix()[:2, :2])
+        marker.points = [Point(x=x,y=y) for (x, y) in particles_base_laser.tolist()]
+        marker.frame_locked = True
+        self.marker_pdf_publisher.publish(marker)
+
+    def ros2_to_pose(self, odom_transform):
+        trans_tf = odom_transform.transform.translation
+        rot_tf = odom_transform.transform.rotation
+        rot = [rot_tf.x, rot_tf.y, rot_tf.z, rot_tf.w]
+        _, _, theta = euler_from_quaternion(rot)
+        return (trans_tf.x, trans_tf.y, theta)
+
+    def publish_ros2(self, tf_base_laser_to_odom, pose):
+        self.publish_map_odom_transform(tf_base_laser_to_odom, pose)
+        self.publish_particles(pose)
+
+    def robot_moved(self, current_odom_pose):
+        diff_x = current_odom_pose[0] - self.previous_odom_pose[0]
+        diff_y = current_odom_pose[1] - self.previous_odom_pose[1]
+        d_trans = np.sqrt(diff_y**2 + diff_x**2)
+        diff_angle = angle_diff(current_odom_pose[2], self.previous_odom_pose[2])
+        return np.abs(diff_angle) > self.min_angle or d_trans > self.min_dist
+
+    def lidar_callback(self, lidar_msg):
+        if self.init_wait < 10:
+            self.init_wait += 1
+            return
+        if self.current_lidar_msg is None:
+            self.current_lidar_msg = lidar_msg
+            return
+        lidar_msg_time = Time.from_msg(self.current_lidar_msg.header.stamp)
+        try:
+            tf_base_laser_to_odom = self.tf_buffer.lookup_transform(
+                "base_laser",
+                "odom",
+                lidar_msg_time)  # https://github.com/ros2/ros2_documentation/issues/4385
+            tf_odom_to_base_laser = self.tf_buffer.lookup_transform(
+                "odom",
+                "base_laser",
+                lidar_msg_time)
+        except TransformException as ex:  # This is common and normal.
+            return
+        self.process_lidar(self.current_lidar_msg, tf_base_laser_to_odom, tf_odom_to_base_laser)
+        self.current_lidar_msg = None
+
+    def process_lidar(self, lidar_msg, tf_base_laser_to_odom, tf_odom_to_base_laser):
+        scan = np.array(lidar_msg.ranges)
+        current_odom_pose = self.ros2_to_pose(tf_odom_to_base_laser)
+        if self.init_wait == 10:
+            self.slam.init(scan, current_odom_pose)
+            self.init_wait += 1
+            return
+        if self.previous_odom_pose is None:
+            self.previous_odom_pose = current_odom_pose
+        if self.robot_moved(current_odom_pose):
+            self.slam.update(np.array(lidar_msg.ranges), self.previous_odom_pose, current_odom_pose)
+            self.previous_odom_pose = current_odom_pose
+        pose = self.slam.expected_pose()
+        self.publish_ros2(tf_base_laser_to_odom, pose)
+
+
+rclpy.init()
+slam_node = SLAMNode()
+rclpy.spin(slam_node)
+slam_node.destroy_node()
+rclpy.shutdown()
